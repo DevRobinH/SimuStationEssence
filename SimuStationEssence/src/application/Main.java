f6213d3c57e7ec96d7af373db1b07a6a228fbfba
Florent <Florent@LAPTOP-AB99RUP6>
2019-05-08 11:00:46 +0200

    IHM : Logo et titre

--- SimuStationEssence/src/application/Main.java
+++ SimuStationEssence/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -13,6 +14,13 @@ public class Main extends Application {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Station.fxml"));
 			Scene scene = new Scene(root,1200,600);
+			
+			// Titre de la fenetre
+			primaryStage.setTitle("Station Service Carburant");
+			
+			// Logo 3iL
+			primaryStage.getIcons().setAll(new Image(getClass().getResource("3ilogo.png").toExternalForm()));
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
